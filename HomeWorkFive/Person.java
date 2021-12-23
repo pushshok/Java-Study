11ace52613184a2f716c0ef990caa0dbc6a33212
pushshok <pushshok@gmail.com>
2021-12-23 18:41:20 +0300

    Создана новая ветка, добавлен файл с кодом по домашнему заданию N5 и внесены измененения в файл README.md

---
+++ HomeWorkFive/Person.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.home_work_app;
+
+public class Person {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private float salary;
+    private int age;
+
+    public static void main(String[] args) {
+    }
+
+    Person(String fullName, String position, String email, String phone, float salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return ("Full Name: " + fullName + "; Position: " + position + "; Email: " + email + "; Phone: " + phone + "; Salary: " + salary + "; Age: " + age);
+    }
+}
---
+++ HomeWorkFive/SelectPerson.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.home_work_app;
+import ru.geekbrains.home_work_app.Person;
+
+public class SelectPerson {
+
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30); // потом для каждой ячейки массива задаем объект
+        persArray[1] = new Person("Petrov Pavel", "Teacher", "pepavel@mailbox.com", "892312334", 30000, 25);
+        persArray[2] = new Person("Sidorov Ivan", "Manager", "siivan@mailbox.com", "8923124512", 30000, 40);
+        persArray[3] = new Person("Volkov Vladimir", "Worker", "vovlad@mailbox.com", "892312319", 30000, 20);
+        persArray[4] = new Person("Zaycev Stas", "Programmer", "zastas@mailbox.com", "892372312", 30000, 55);
+
+        for (int i = 0; i < 5; i++) {
+            if (persArray[i].getAge() >= 40) {
+                System.out.println(persArray[i].toString());
+            }
+        }
+    }
+}
